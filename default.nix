2c598e492ffa25a38a197ebc202339ad5236bab4
Ruben Louis <rubenlouis@MacBook-Pro-de-Ruben.local>
2018-05-17 23:29:37 +0400

    some more more experimentation

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ self: super: with self; {
   python27Packages = super.recurseIntoAttrs (python27.pkgs);
   python36 = super.python36.override pythonOverrides;
   python36Packages = super.recurseIntoAttrs (python36.pkgs);
-  
+
   pythonOverrides = {
     packageOverrides = python-self: python-super: {
       flask-helloworld = pythonPackages.buildPythonPackage rec {
@@ -11,9 +11,9 @@ self: super: with self; {
         version = "0.1.0";
 
         src = fetchgit {
-          url = "https://github.com/knedlsepp/flask-hello-world.git";
-          rev = "dff5896234ce2bd7afa66134206f3403f2d94e38";
-          sha256 = "0g350ikvnz1kzyb31z6363nvjgyfj5f75a2c1p008s18sv0blqr3";
+          url = "https://github.com/zenbur/flask-hello-world";
+          rev = "59ce6930a3cf6d5f25c8d40de246440d3ffd17f8";
+          sha256 = "0b4sy25kf9x0zhlzz4h3r97gjjmmpicjxkw4frshlahv8prvhmlw";
         };
         propagatedBuildInputs = with pythonPackages; [
           flask
